e9c6250616911e24cc086b29a8e6bc4b918028fd
Matheus Castello <matheus@castello.eng.br>
2025-11-09 19:36:32 -0300

    recipes-init: syna-shit: Fix fbset path
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-init/syna-shit/busybox/00-modeset.sh
+++ cookbook/recipes-init/syna-shit/busybox/00-modeset.sh
@@ -2,7 +2,7 @@
 
 if [[ $MACHINE == sl2619 ]]; then
     echo "[initramfs] Synaptics modeset"
-    /usr/bin/fbset -g 1920 1080 1920 1080 32
+    /bin/fbset -g 1920 1080 1920 1080 32
 
 else
     echo "[initramfs] Synaptics modeset not needed"
