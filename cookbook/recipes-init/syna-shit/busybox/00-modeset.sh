7be67a9384eb044e956e8a3e3ee49459cf4c2fc7
Matheus Castello <matheus@castello.eng.br>
2025-11-09 19:56:46 -0300

    recipes-init: syna-shit: Add some time to settle the modeset
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-init/syna-shit/busybox/00-modeset.sh
+++ cookbook/recipes-init/syna-shit/busybox/00-modeset.sh
@@ -4,6 +4,9 @@ if [[ $MACHINE == sl2619 ]]; then
     echo "[initramfs] Synaptics modeset"
     /bin/fbset -g 1920 1080 1920 1080 32
 
+    # let's settle
+    sleep 4
+
 else
     echo "[initramfs] Synaptics modeset not needed"
     exit 69
